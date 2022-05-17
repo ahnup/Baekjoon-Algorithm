b913ce6b1145223bd53a3b41151d9efcdf7ef693
Sangyeon <sy990607@naver.com>
2022-05-17 17:37:33 +0900

    baekjoon: 10869
    백준 10869 : 사칙연산

---
+++ python/baekjoon_10869.py
@@ -0,0 +1,4 @@
+a, b = map(int, input().split())
+print(a+b, a-b, a*b, a//b, a%b, sep="\n")
+# print("%d\n%d\n%d\n%d\n%d" % (a+b, a-b, a*b, a/b, a%b))
+# python print문 option : sep, end, format(.format, %) 
