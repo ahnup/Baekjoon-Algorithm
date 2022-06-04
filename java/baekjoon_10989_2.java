f3d3a7272edf8b133f964c326b44409119ad4117
Sangyeon <sy990607@naver.com>
2022-06-04 11:45:43 +0900

    baekjoon: 10989 (2)
    백준 10989 : 수 정렬하기 3 (CountingSort)

--- java/baekjoon_10989_2.java
+++ java/baekjoon_10989_2.java
@@ -1,20 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int num[] = new int[n];
+        int num[] = new int[10001];
         for(int i=0; i<n; i++){
-            num[i] = Integer.parseInt(br.readLine());
+            num[Integer.parseInt(br.readLine())] += 1;
         }
-        Arrays.sort(num);
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<n; i++){
-            sb.append(num[i]+" ");
+        for(int i=0; i<10001; i++){
+            if(num[i] != 0){
+                for(int j=0; j<num[i]; j++){
+                    sb.append(i+"\n");
+                }
+            }
         }
         System.out.println(sb);
     }
